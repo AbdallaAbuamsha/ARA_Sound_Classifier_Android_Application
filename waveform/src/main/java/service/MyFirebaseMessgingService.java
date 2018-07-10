4e166c00104919c4df3d07ffe23f4ebc375ffd5f
DESKTOP-E5QJJ8R\ranop <ranoprog@gmail.com>
2018-07-10 08:25:13 +0300

    notification files added to the project

---
+++ waveform/src/main/java/service/MyFirebaseMessgingService.java
@@ -0,0 +1,17 @@
+package service;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+
+import app.Config;
+
+/**
+ * Created by Ravi Tamada on 08/08/16.
+ * www.androidhive.info
+ */
+public class MyFirebaseInstanceIDService {
+
+}
